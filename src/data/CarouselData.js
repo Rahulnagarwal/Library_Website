279baa3c4f3e5f185fac4a73e0470f61e202151e
rahulNagarwal <rahul.nagarwal@rsvrtech.com>
2022-07-12 22:36:22 +0530

    description updated

--- src/data/CarouselData.js
+++ src/data/CarouselData.js
@@ -2,31 +2,31 @@ export const data = [
 	{
 		title: 'Thinking, Fast and Slow',
 		description:
-			'“Intelligence is not only the ability to reason; it is also the ability to find relevant material in memory and to deploy attention when needed.”',
+			'Intelligence is not only the ability to reason; it is also the ability to find relevant material in memory and to deploy attention when needed.',
 		image: './assets/thinkingFast.webp',
 		price: 'Price : ₹500'
 	},
 	{
 		title: 'जीत आपकी',
-		description: 'Our team consists of the best experts in the industry, learn about them',
+		description: 'मुझे यकीन है कि जीत हासिल करनें वालों के मन में भी टालमटोल करनें कि इच्छा जरूर पैदा हुई होगी, पर उन्होंने उसे अपनें ऊपर कभी हावी होने नहीं दिया |',
 		image: './assets/jeet.webp',
 		price: 'Price : ₹400'
 	},
 	{
 		title: 'Atomic Habits',
-		description: 'Learn more about our security systems to make sure your data is always safe',
+		description: 'If you can get 1% better each day for one year, you’ll end up 37 times better by the time you’re done.',
 		image: './assets/images.jfif',
 		price: 'Price : ₹800'
 	},
 	{
 		title: 'युगद्रष्टा विवेकानन्द ',
-		description: 'Find more about hardware and software used for your service',
+		description: 'We are what our thoughts have made us; so take care about what you think. Words are secondary. Thoughts live; they travel far.',
 		image: './assets/vivekanand.jpg',
 		price: 'Price : ₹200'
 	},
 	{
 		title: '21 Things Every Girl Should Know',
-		description: 'We have provided services to top clients in tech industry',
+		description: "A daughter is a mother's gender partner, a part of herself that resembles her the most.",
 		image: './assets/21.jfif',
 		price: 'Price : ₹500'
 	},
